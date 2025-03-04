46e3e8385e046b9e898ee0a05c7cc8388dcc2385
piyushappinessworld <piyush.a@appinessworld.com>
2025-03-04 16:45:53 +0530

    bug fixes

--- backend/src/routes/property.routes.js
+++ backend/src/routes/property.routes.js
@@ -1,21 +1,20 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { protect } = require('../middleware/auth.middleware');
-const { upload } = require('../config/s3.config');
+const { protect } = require("../middleware/auth.middleware");
+const { upload } = require("../config/s3.config");
 const {
   createProperty,
   getProperties,
   getProperty,
   updateProperty,
-  deleteProperty
-} = require('../controllers/property.controller');
+  deleteProperty,
+} = require("../controllers/property.controller");
 
 // Routes
-router.post('/', protect, upload.array('images', 5), createProperty);
-router.get('/', getProperties);
-router.get('/:id', protect, getProperty);
-router.put('/:id', protect, upload.array('images', 5), updateProperty);
-router.delete('/:id', protect, deleteProperty);
+router.post("/", protect, upload.array("images", 10), createProperty);
+router.get("/", getProperties);
+router.get("/:id", getProperty);
+router.put("/:id", protect, upload.array("images", 10), updateProperty);
+router.delete("/:id", protect, deleteProperty);
 
 module.exports = router;
