2553df0acf07d5ae34a293ac481b14908af5df00
piyushappinessworld <piyush.a@appinessworld.com>
2025-03-04 16:34:24 +0530

    bug fixes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://suparnassa-admin-backend.onrender.com/api',
+  baseURL: 'https://suparnassa-admin.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
